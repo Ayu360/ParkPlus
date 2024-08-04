7f9be1dcc8c3131da99118293d3bb190c5887f75
Ayush Singh <ayushandsingh987654321@gmail.com>
2024-08-04 15:51:48 +0530

    Everything configured

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import { WebView } from "react-native-webview";
-import Constants from "expo-constants";
 import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <WebView
-      style={styles.container}
-      source={{ uri: "https://parkplus.io/" }}
-    />
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: "rgb(84, 46, 187)", marginBottom: 0 }}
+    >
+      <WebView
+        style={styles.container}
+        source={{ uri: "https://parkplus.io/" }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
+    //marginTop: Constants.statusBarHeight,
   },
 });
